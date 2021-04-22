c2cf4675012fc7d73053ba401b263dc8e480f0a0
acer-PC\acer <kienphat10@gmail.com>
2021-04-22 19:59:20 +0700

    innit

--- js/my-script.js
+++ js/my-script.js
@@ -61,4 +61,75 @@ $(document).ready(function () {
 		});
 	}
 
+	let playNews = `${urlPlaylists}/1/videos`;
+	$.get(playNews, { limit: 12 }, function (data) {
+		let content = ''
+		data.forEach(element => {
+			let thumb = JSON.parse(element.thumbnail).high.url;
+			let title = element.title;
+			content += `
+			
+			<div class="col-6 col-sm-4 col-lg-3 col-xl-2">
+					<div class="card">
+						<div class="card__cover">
+							<img src="${thumb}" alt="${title}">
+							<a href="#" class="card__play">
+								<i class="icon ion-ios-play"></i>
+							</a>
+						</div>
+						<div class="card__content">
+							<h3 class="card__title"><a href="#">${title}</a></h3>
+							<span class="card__category">
+								<a href="#">Action</a>
+								<a href="#">Triler</a>
+							</span>
+							<span class="card__rate"><i class="icon ion-ios-star"></i>8.4</span>
+						</div>
+					</div>
+			</div>
+			`
+			
+		})
+		$('#playPlistsNews > div:first-child').after(content);
+	})
+
+	$.get(urlPlaylists, { limit: 3 }, function (data) {
+		let content = '';
+		let contentCatagory = '';
+		data.forEach(element => {
+			let thumb = JSON.parse(element.thumbnail).high.url;
+			let title = element.title;
+			content += `
+			<li class="nav-item">
+				<a class="nav-link active" data-toggle="tab" href="#tab-2" role="tab" aria-controls="tab-2" aria-selected="false">${title}</a>
+			</li>
+			`
+
+			contentCatagory += `
+				<div class="card card--big">
+					<div class="card__cover">
+						<img src="${thumb}" alt="${title}">
+						<a href="#" class="card__play">
+							<i class="icon ion-ios-play"></i>
+						</a>
+					</div>
+					<div class="card__content">
+						<h3 class="card__title"><a href="#">${title}</a></h3>
+						<span class="card__category">
+							<a href="#">Romance</a>
+							<a href="#">Drama</a>
+						</span>
+						<span class="card__rate"><i class="icon ion-ios-star"></i>6.3</span>
+					</div>
+				</div>
+			`
+		});
+
+		$('#content__tabs').html(content);
+		$('#playVideosId').html(content);
+		
+	});
+
 });
+
+
